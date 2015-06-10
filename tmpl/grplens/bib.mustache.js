41fd7de822400764f4e1073b9b9d9b087d8fa947
Max Harper <maxharp3r@gmail.com>
2015-06-10 16:50:42 -0500

    use a modal display to show details

--- tmpl/grplens/bib.mustache.js
+++ tmpl/grplens/bib.mustache.js
@@ -7,7 +7,6 @@ $(document).ready(function() {
 
   // initialize list.js
   var pubList = new List('zth-pubs', {
-    // indexAsync: true,
     page: 2000,
     valueNames: ['bib-entry-summary']
   });
